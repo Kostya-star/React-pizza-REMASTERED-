6f752575e2ad5987de204254ea64a3514e59af7a
Constantin <konstantindanilov19@hotmail.com>
2023-01-27 13:40:26 +0200

    started creating search

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { Cart } from 'pages/Cart';
 import { Home } from 'pages/Home';
 import { NotFound } from 'pages/NotFound';
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import './scss/app.scss';
 
 const App = () => {
+  const[searchVal, setSearchVal] = useState()
   return (
     <div className="wrapper">
       <div className="container">
-        <Header />
+        <Header searchVal={searchVal} setSearchVal={setSearchVal}/>
         <div className="content">
           <Routes>
-            <Route path="" element={<Home />} />
+            <Route path="" element={<Home searchVal={searchVal}/>} />
             <Route path="cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
