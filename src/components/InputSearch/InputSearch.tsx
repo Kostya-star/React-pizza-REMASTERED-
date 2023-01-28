d8d0ef888a7b21d66f2cf8ee060ffbdb327b8c50
Constantin <konstantindanilov19@hotmail.com>
2023-01-28 21:10:59 +0200

    added loadsh debounce for the input search

--- src/components/InputSearch/InputSearch.tsx
+++ src/components/InputSearch/InputSearch.tsx
@@ -1,23 +1,30 @@
 import { ReactComponent as CloseSVG } from 'assets/svg/close.svg';
 import { ReactComponent as SearchGlassSVG } from 'assets/svg/search-glass.svg';
-import { FC, useState } from 'react';
+import { FC, useRef, useState, useCallback } from 'react';
 import { setSearchValue } from 'redux/slices/homeSlice';
-import s from './InputSearch.module.scss';
 import { useAppDispatch } from './../../redux/hooks';
+import s from './InputSearch.module.scss';
+import debounce from 'lodash.debounce';
 
 export const InputSearch: FC = () => {
   const [value, setValue] = useState('');
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
+  const searchRef = useRef<HTMLInputElement>(null);
+
+  const debouncedSearch = useCallback(debounce((value: string) => {
+    dispatch(setSearchValue(value))
+  }, 700), [])
 
   const onSearchChange = (value: string) => {
     setValue(value);
-    dispatch(setSearchValue(value));
+    debouncedSearch(value)
   };
 
   const onClearSearch = () => {
-    setValue('')
-    dispatch(setSearchValue(''))
-  }
+    setValue('');
+    dispatch(setSearchValue(''));
+    searchRef.current?.focus();
+  };
 
   return (
     <div className={s.search}>
@@ -26,6 +33,7 @@ export const InputSearch: FC = () => {
         value={value}
         onChange={(e) => onSearchChange(e.target.value)}
         type="text"
+        ref={searchRef}
         placeholder="Search"
       />
       {value && <CloseSVG onClick={onClearSearch} />}
