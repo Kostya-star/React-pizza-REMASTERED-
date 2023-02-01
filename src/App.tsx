e349cc05f1bb88d136654ddc3809f132e8a3fbc4
Constantin <konstantindanilov19@hotmail.com>
2023-02-01 21:17:49 +0200

    started working on pizzadetails page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
           <Routes>
             <Route path="" element={<Home />} />
             <Route path="cart" element={<Cart />} />
+            {/* <Route path="cart" element={<Cart />} /> */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
---
+++ src/pages/PizzaDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const PizzaDetails = () => {
+  return (
+    <div>
+      <img src="" alt="pizza" />
+      <h2>pizza name</h2>
+      <span>pizza price</span>
+    </div>
+  )
+}
