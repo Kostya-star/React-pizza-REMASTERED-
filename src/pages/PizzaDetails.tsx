ec6954bb9ce077e7dda7b3386aeeb1e3c3995704
Constantin <konstantindanilov19@hotmail.com>
2023-02-02 11:34:38 +0200

    built the pizza details section

--- src/pages/PizzaDetails.tsx
+++ src/pages/PizzaDetails.tsx
@@ -1,24 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import { baseRequest } from 'api/baseRequest';
 import axios from 'axios';
 import qs from 'qs';
+import { IPizza } from 'types/types';
 
 export const PizzaDetails = () => {
+  const [pizza, setPizza] = useState<IPizza | null>(null)
   const { id } = useParams()
+
   useEffect(() => {
     const fetchPizzaById = async () => {
-      const resp = await axios.get(`${baseRequest}/${id}`);
-      console.log(resp);
+      const resp = await axios.get(`${baseRequest}/${id}`).catch(e => {
+        console.log(e);        
+      })
+      if(resp?.status === 200) {
+        setPizza(resp.data)
+      }        
     };
     void fetchPizzaById();
   }, []);
 
   return (
-    <div>
-      <img src="" alt="pizza" />
-      <h2>pizza name</h2>
-      <span>pizza price</span>
+    <div className='pizza_details'>
+      <img src={pizza?.imageUrl} alt="pizza" />
+      <h2>{pizza?.title}</h2>
+      <span>{pizza?.price} ₽ rub</span>
     </div>
   );
 };
