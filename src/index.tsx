3ea77ee71f3ef03dd2f26cc5579bd027a91a6715
Constantin <konstantindanilov19@hotmail.com>
2023-02-03 14:16:28 +0200

    implemented redux-persist and made cart persistable after reloading

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { store } from 'redux/store';
+import { store, persistor } from 'redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>,
 );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,48 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import home from './slices/homeSlice';
 import cart from './slices/cartSlice';
 import pizzaDetails from './slices/pizzaDetailsSlice';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({
+  home,
+  cart,
+  pizzaDetails
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['home', 'pizzaDetails']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    home,
-    cart,
-    pizzaDetails
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  // reducer: {
+  //   home,
+  //   cart,
+  //   pizzaDetails
+  // },
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
